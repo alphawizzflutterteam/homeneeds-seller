a38529a4cb4a6ae269832c333d2685da0ad46ff7
Alphawizz Flutter Team <138693166+alphawizzflutterteam@users.noreply.github.com>
2025-07-21 15:47:05 +0530

    sound notification bug fix commit by anjali

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter_windows_3.7.2-stable\flutter"
+export "FLUTTER_ROOT=C:\src\flutter_windows_3.16.0-stable\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Ankush\homeneeds-seller"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
